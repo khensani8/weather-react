86cf970d0823a24d09a57fc30a1b3980e0bbd78d
Khensani <129982152+khensani8@users.noreply.github.com>
2024-07-03 12:34:30 +0200

    created a react app for practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from App</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -13,8 +14,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
