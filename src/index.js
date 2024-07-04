9c800612c513ad38cf5e150c7dd369d9b353943b
Khensani <129982152+khensani8@users.noreply.github.com>
2024-07-04 16:00:59 +0200

    Created a weather search component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Weather from "./Weather"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import WeatherSearch from "./WeatherSearch";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <Weather />
+    <h1>Weather Search App</h1>
+    <WeatherSearch />
   </React.StrictMode>
 );
 
